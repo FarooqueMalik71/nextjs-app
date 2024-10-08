13ab5da0f459ba7c81e2b53103060bb0fda9f7fe
FarooqueMalik71 <farooquemalik50871@gmail.com>
2024-10-09 00:50:48 +0500

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+'use client'
 import Click from "@/components/Click";
 
 import Image from "next/image";
 
 export default function Home() {
+
+
+  const handleClick = ()=> {
+
+
+    alert('💕')
+  }
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -14,12 +22,19 @@ export default function Home() {
           height={38}
           priority
         />
-        <Click/>
+        <Click />
+        <button  onClick={handleClick}>Click me</button>
+
+
+        {/* <div className=" rounded-sm text-black h-24 w-14 bg-slate-300 "><h1>h1</h1></div>
+
+        <Image src={'/public/IMG_20230824_075009'} alt="profile image"></Image> */}
+
       </main>
 
 
-     </div>
+    </div>
 
-     
+
   );
 }
